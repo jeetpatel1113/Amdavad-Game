9cb5711ea45fa16d37e7b1b1205deeb0d59b8a0f
jeetpatel1113 <jmpatel7238@gmail.com>
2025-09-06 08:32:48 -0700

    Deleted all

--- client/src/socket.js
+++
@@ -1,5 +0,0 @@
-// client/src/socket.js
-import { io } from "socket.io-client";
-
-const URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:3000";
-export const socket = io(URL, { transports: ["websocket"] });
